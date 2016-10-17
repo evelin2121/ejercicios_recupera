4874546e213232c2f01912fc77050dd4d141ca8e
evelin <evelin.212122@gmail.cl>
2016-10-17 09:58:54 -0300

    ..

---
+++ pagina 1/sueldo/js.js
@@ -0,0 +1,13 @@
+function promedio ( nu1, nu2, nu3){
+    
+
+	var nu1= 6;
+	var nu2= 4;
+	var nu3= 5;
+	var resultado= 3;
+     
+     return nu1 + nu2 + nu3 / resultado;
+ }
+
+ 
+ document.write( promedio(6,4,5/3))
---
+++ pagina 1/suma 2 numeros/js.js
@@ -0,0 +1,9 @@
+function suma (numero1, numero2){
+
+	var numero1= 3;
+	var numero2= 5;
+     
+     return numero1 + numero2;
+ }
+
+  document.write( suma ( 3,5 ));
